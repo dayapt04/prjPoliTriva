fc0a3c2ab5cc7bb69c6f40765e2e0c1b51ec5710
dayapt04 <daya.pt04@gmail.com>
2024-02-26 21:34:05 -0500

    Merge Liam

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+// import BusinessLogic.Entities.LZCodigoQR;
+// import BusinessLogic.Entities.LZLectorQR;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // String data =
+        // "https://epnecuador-my.sharepoint.com/:u:/g/personal/liam_zuniga_epn_edu_ec/EW-BygwtvO1Gjs9z07YvKTsB48faMDLI3GAxkYjIfnCI-g?e=qSeTWj";
+        // // Cambia
+        // // esto
+        // // por
+        // // el
+        // // enlace
+        // // que
+        // // desees
+        // // codificar
+        // String filePath = "qr_code.png";
+        // LZCodigoQR.generateQR(data, filePath);
+        // LZLectorQR.mostrarImagenQR(filePath);
     }
 }
--- src/BusinessLogic/Entities/LZCodigoQR.java
+++ src/BusinessLogic/Entities/LZCodigoQR.java
@@ -1,5 +1,34 @@
 package BusinessLogic.Entities;
 
-public class LZCodigoQR {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import java.awt.image.BufferedImage;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+
+public class LZCodigoQR {
+    public static void generateQR(String data, String filePath) {
+        int width = 300;
+        int height = 300;
+        String format = "png";
+
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, width, height);
+            Path path = FileSystems.getDefault().getPath(filePath);
+            BufferedImage image = MatrixToImageWriter.toBufferedImage(bitMatrix);
+            ImageIO.write(image, format, new File(path.toString()));
+            System.out.println("Código QR generado con éxito.");
+        } catch (WriterException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
