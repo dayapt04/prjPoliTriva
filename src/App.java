ebc66b2624bd818b78934d8f3c45a014f21dbd4f
dayapt04 <daya.pt04@gmail.com>
2024-02-26 21:24:08 -0500

    Qr Update

--- src/App.java
+++ src/App.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
 
 import BusinessLogic.Entities.LZCodigoQR;
-import BusinessLogic.Entities.LectorQR;
-import DataAccess.PreguntaDAO;
+import BusinessLogic.Entities.LZLectorQR;
+// import DataAccess.PreguntaDAO;
 
 public class App {
-    private static Scanner sc = new Scanner(System.in);
+    // private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
 
         // PreguntaDAO pDAO = new PreguntaDAO();
         // // System.out.println(pDAO.readAll().toString());
         // for (int i = 1; i <= 25; i++) {
-        //     System.out.println(pDAO.readBy(i).toString());
-        //     sc.nextLine();
+        // System.out.println(pDAO.readBy(i).toString());
+        // sc.nextLine();
         // }
 
-        String data = "https://epnecuador-my.sharepoint.com/:u:/g/personal/liam_zuniga_epn_edu_ec/EW-BygwtvO1Gjs9z07YvKTsB48faMDLI3GAxkYjIfnCI-g?e=qSeTWj"; // Cambia esto por el enlace que desees codificar
+        String data = "https://epnecuador-my.sharepoint.com/:u:/g/personal/liam_zuniga_epn_edu_ec/EW-BygwtvO1Gjs9z07YvKTsB48faMDLI3GAxkYjIfnCI-g?e=qSeTWj"; // Cambia
+                                                                                                                                                            // esto
+                                                                                                                                                            // por
+                                                                                                                                                            // el
+                                                                                                                                                            // enlace
+                                                                                                                                                            // que
+                                                                                                                                                            // desees
+                                                                                                                                                            // codificar
         String filePath = "qr_code.png";
         LZCodigoQR.generateQR(data, filePath);
-        LectorQR.mostrarImagenQR(filePath);
+        LZLectorQR.mostrarImagenQR(filePath);
     }
 }
