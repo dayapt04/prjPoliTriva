1af3c0e5a7741c93f867089b20d45b2c5e8c7c7d
dayapt04 <daya.pt04@gmail.com>
2024-02-26 21:41:14 -0500

    Part 1 Merge Daya2

---
+++ Script/DDL_PoliTrivia.sql
@@ -0,0 +1,99 @@
+-- database: ../database/POLITRIVIA.sqlite
+/*
+|----------------------------------------|
+| (©) 2K24 EPN-FIS, All rights reserved. |
+| pat_mic@hotmail.com           pat_mic  |
+|----------------------------------------|
+Autor : Karolina Salazar
+Fecha : 24.feb.2k24
+Script: Creacion de la tabla preguntas + datos
+*/
+-- Tabla de categorías de preguntas
+DROP TABLE IF EXISTS CategoriaEstructura;
+
+CREATE TABLE
+    CategoriaEstructura (
+        IdCategoriaEstructura INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        IdCategoriaEstructuraPadre INTEGER REFERENCES CategoriaEstructura (IdCategoriaEstructura),
+        Nombre VARCHAR(50) NOT NULL,
+        Descripcion VARCHAR(255) DEFAULT NULL,
+        Estado VARCHAR(1) NOT NULL DEFAULT ('A'),
+        FechaCrea DATETIME DEFAULT (datetime('now', 'localtime')),
+        FechaModifica DATETIME
+    );
+
+DROP TABLE IF EXISTS Pregunta;
+
+CREATE TABLE
+    Pregunta (
+        IdPregunta INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        IdCategoriaEstructura INTEGER NOT NULL REFERENCES CategoriaEstructura (IdCategoriaEstructura),
+        Enunciado TEXT NOT NULL,
+        Estado VARCHAR(1) NOT NULL DEFAULT ('A'),
+        FechaCrea DATETIME DEFAULT (datetime('now', 'localtime')),
+        FechaModifica DATETIME
+    );
+
+-- Tabla de etiquetas de preguntas
+DROP TABLE IF EXISTS Respuesta;
+
+CREATE TABLE
+    Respuesta (
+        IdRespuesta INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        IdPregunta INTEGER NOT NULL REFERENCES Pregunta (IdPregunta),
+        RespuestaUsuario TEXT NOT NULL,
+        Aprobada BOOLEAN DEFAULT 0,
+        FechaCrea DATETIME DEFAULT (datetime('now', 'localtime')),
+        FechaModifica DATETIME
+    );
+
+DROP TABLE IF EXISTS EtiquetaPregunta;
+
+-- CREATE TABLE
+--     EtiquetaPregunta (
+--         IdEtiquetaPregunta INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+--         Nombre VARCHAR(50) NOT NULL,
+--         Descripcion VARCHAR(255) DEFAULT NULL,
+--         Estado VARCHAR(1) NOT NULL DEFAULT ('A'),
+--         FechaCrea DATETIME DEFAULT (datetime('now', 'localtime')),
+--         FechaModifica DATETIME
+--     );
+-- -- Tabla de relaciones entre preguntas y etiquetas
+DROP TABLE IF EXISTS PreguntaEtiqueta;
+
+-- CREATE TABLE
+--     PreguntaEtiqueta (
+--         IdPregunta INTEGER NOT NULL REFERENCES Pregunta (IdPregunta),
+--         IdEtiquetaPregunta INTEGER NOT NULL REFERENCES EtiquetaPregunta (IdEtiquetaPregunta),
+--         Estado VARCHAR(1) NOT NULL DEFAULT ('A'),
+--         FechaCrea DATETIME DEFAULT (datetime('now', 'localtime')),
+--         FechaModifica DATETIME
+--     );
+SELECT
+    *
+FROM
+    CategoriaEstructura
+WHERE
+    Estado = "A";
+
+SELECT
+    *
+FROM
+    Pregunta
+WHERE
+    Estado = "A";
+
+SELECT    *
+FROM Respuesta
+WHERE   IdPregunta = 3;
+
+SELECT
+    p.IdPregunta PreguntaId,
+    p.Enunciado Pregunta,
+    cp.Nombre Categoria
+FROM
+    Pregunta p
+    JOIN CategoriaEstructura cp ON p.IdPregunta = cp.IdCategoriaEstructura
+WHERE
+    p.Estado = "A"
+    AND cp.Estado = "A";
